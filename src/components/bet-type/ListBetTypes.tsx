9ffc2b565ed22faf3d8ec7ad30a6b9e120a06e11
Saurabh Bansal <saurabhba@gmail.com>
2023-02-13 09:31:28 +0100

    fix for failed build

--- src/components/bet-type/ListBetTypes.tsx
+++ src/components/bet-type/ListBetTypes.tsx
@@ -1,23 +1,26 @@
 import { Tab, Tabs } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { BetType } from "./types";
 
 export const ListBetTypes = () => {
-  const BET_TYPE_DATA: BetType[] = [
-    {
-      label: "V75",
-      to: "/V75",
-    },
-    {
-      label: "V86",
-      to: "/V86",
-    },
-    {
-      label: "GS75",
-      to: "/GS75",
-    },
-  ];
+  const BET_TYPE_DATA: BetType[] = useMemo(
+    () => [
+      {
+        label: "V75",
+        to: "/V75",
+      },
+      {
+        label: "V86",
+        to: "/V86",
+      },
+      {
+        label: "GS75",
+        to: "/GS75",
+      },
+    ],
+    []
+  );
 
   const [selectedBetType, setSelectedBetType] = useState(0);
   const location = useLocation();
@@ -28,11 +31,11 @@ export const ListBetTypes = () => {
       (betType) => betType.to === location.pathname
     );
     if (index > 0) setSelectedBetType(index);
-  }, [location.pathname, BET_TYPE_DATA]);
+  }, [location.pathname, BET_TYPE_DATA, setSelectedBetType]);
 
   useEffect(() => {
     if (location.pathname === "/") navigate(BET_TYPE_DATA[selectedBetType].to);
-  }, [location.pathname, BET_TYPE_DATA]);
+  }, [location.pathname, BET_TYPE_DATA, navigate, setSelectedBetType]);
 
   return (
     <>
